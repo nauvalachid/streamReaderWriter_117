6884cd2ca797b069a0f5d931115848a6fdb08963
nauvalachid <nauvalachid@gmail.com>
2023-06-19 10:39:02 +0700

    jika file ada

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -29,3 +29,7 @@ int main () {
 	ifstream infile;
 	//menunjuk ke sebuah file
 	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
