183e8c25a0a8f19a2548f9bdd248aee7a6c6f52d
nauvalachid <nauvalachid@gmail.com>
2023-06-19 10:33:26 +0700

    loop akan berhenti jika anda memasukkan karakter 4

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -17,3 +17,5 @@ int main () {
 		cout << "_";
 		//mendapatkan setiap karakter dalam satu baris
 		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan karakter 4
+		if (baris == "q") break;
