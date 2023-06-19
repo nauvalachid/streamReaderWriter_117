d6ebf9289140bce2aecb8dcca89ab577b2e15d04
nauvalachid <nauvalachid@gmail.com>
2023-06-19 11:09:29 +0700

    tutup file setelah selesai

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -36,3 +36,15 @@ int main() {
 	infile.open(NamaFile, ios::in);
 
 	cout << endl << ">=Membuka dan membaca file" << endl;
+	//jika file ada maka 
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan disini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
