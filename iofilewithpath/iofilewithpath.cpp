d3b2e0c7652eb4ac7dabaab8fcb4d9353ab1b8f5
nauvalachid <nauvalachid@gmail.com>
2023-06-19 11:02:49 +0700

    membenarkan menulis dan memasukkan nilai dari 'baris' ke dalam file

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -25,5 +25,5 @@ int main() {
 		//loop akan berhenti jika anda memasukkan karakter q
 		if (baris == "q") break;
 		//menulis dan memasukkan nilai dari 'baris' ke dalam file
-		outfile << baris << endl
+		outfile << baris << endl;
 	}
