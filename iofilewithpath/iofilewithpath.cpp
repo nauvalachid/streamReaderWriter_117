f4ce6500854052c61dfd057690f6ee857af574ee
nauvalachid <nauvalachid@gmail.com>
2023-06-19 10:53:24 +0700

    membuka file dalam mode menulis

---
+++ iofilewithpath/iofilewithpath.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+	string NamaFile;
+
+	cout << "Masukan Nama File : ";
+	cin >> NamaFile;
+
+	//membuka file dalam mode menulis.
+	ofstream outfile;
